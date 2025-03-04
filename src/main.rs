6cbaf915984a0c868652c541cbe7d2edc2115dce
Asia4046 <kirito234875@gmail.com>
2025-03-04 12:43:09 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs;
+use std::process::exit;
+
+fn run_file(path: &str) -> Result<(), String> {
+    match fs::read_to_string(path) {
+        Err(msg) => return Err(msg.to_string()), 
+        _ => return Ok(()),
+    }
+    // run(contents);
+}
+
+fn run_prompt(){
+     
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("UsageL jlox [script]");
+        exit(64);
+    } else if args.len() == 2 {
+        match run_file(&args[1]){
+            Ok(_) => exit(0),
+            Err(msg) => {
+                println!("ERROR:\n{}", msg);
+                exit(1);
+            }
+        }
+
+    } else {
+        run_prompt();
+    }
+}
