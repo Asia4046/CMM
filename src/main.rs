48c5300429dd2c23784fadfabe4fa9d85365ad0a
Asia4046 <kirito234875@gmail.com>
2025-03-05 15:13:57 +0000

    basic io optimization

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,51 @@
+mod scanner;
+use crate::scanner::*;
+
 use std::env;
 use std::fs;
-use std::process::exit;
 use std::io::{self, BufRead, Write};
+use std::process::exit;
 
 fn run_file(path: &str) -> Result<(), String> {
     match fs::read_to_string(path) {
-        Err(msg) => return Err(msg.to_string()), 
+        Err(msg) => return Err(msg.to_string()),
         Ok(contents) => return run(&contents),
     }
     // run(contents);
 }
 
-fn run(_contents: &str) -> Result<(), String> {
-    return Err("Not Implemented".to_string());
+fn run(contents: &str) -> Result<(), String> {
+    let scanner = Scanner::new(contents);
+    let tokens = scanner.scan_tokens()?;
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+    return Ok(());
 }
 
 fn run_prompt() -> Result<(), String> {
-    print!("> ");
-    match io::stdout().flush() {
-        Ok(_) => (),
-        Err(_) => return Err("Could not flush stdout".to_string()),
-    }
+    loop {
+        print!("> ");
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => return Err("Could not flush stdout".to_string()),
+        }
 
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-    match handle.read_line(&mut buffer) {
-        Ok(_) => (),
-        Err(_) => return Err("Couldnt Read Line".to_string()),
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        match handle.read_line(&mut buffer) {
+            Ok(n) => {
+                if n <= 1 {
+                    return Ok(());
+                }
+            },
+            Err(_) => return Err("Couldnt Read Line".to_string()),
+        }
+
+        println!("You Wrote: {}", buffer);
     }
-    
-    println!("You Wrote: {}", buffer);
-    Ok(())
 }
 
 fn main() {
@@ -41,14 +55,13 @@ fn main() {
         println!("UsageL jlox [script]");
         exit(64);
     } else if args.len() == 2 {
-        match run_file(&args[1]){
+        match run_file(&args[1]) {
             Ok(_) => exit(0),
             Err(msg) => {
                 println!("ERROR:\n{}", msg);
                 exit(1);
             }
         }
-
     } else {
         match run_prompt() {
             Ok(_) => exit(0),
