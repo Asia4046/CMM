2b49647d97139495087c9c80eda175cba1cc772c
Asia4046 <kirito234875@gmail.com>
2025-03-11 14:05:27 +0000

    scan_tokens()

--- src/scanner.rs
+++ src/scanner.rs
@@ -20,9 +20,13 @@ impl Scanner {
     }
 
     pub fn scan_tokens(self: &mut Self) -> Result<Vec<Token>, String> {
+        let mut errors = vec![];
         while !self.is_at_end() {
             self.start = self.current;
-            self.scan_token()?;
+            match self.scan_token() {
+                Ok(_) => (),
+                Err(msg) => errors.push(msg),
+            }
         }
 
         self.tokens.push(Token {
@@ -31,6 +35,16 @@ impl Scanner {
             literal: None,
             line_number: self.line,
         });
+
+        if errors.len() > 0 {
+            let mut joined = "".to_string();
+            errors.iter().map(|msg| {
+                joined.push_str(&msg);
+                joined.push_str("\n");
+            });
+            return Err(joined);
+        }
+
         Ok(self.tokens.clone())
     }
 
@@ -38,8 +52,50 @@ impl Scanner {
         self.current >= self.source.len() 
     }
 
-    fn scan_token(self: &mut Self) -> Result<Token, String> {
+    fn scan_token(self: &mut Self) -> Result<(), String> {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(LeftParen),
+            ')' => self.add_token(RightParen),
+            '{' => self.add_token(LeftBrace),
+            '}' => self.add_token(RightBrace),
+            ',' => self.add_token(Comma),
+            '.' => self.add_token(Dot),
+            '-' => self.add_token(Minus),
+            '+' => self.add_token(Plus),
+            ';' => self.add_token(Semicolon),
+            '*' => self.add_token(Star),
+            _ => return Err(format!("Unrecognised char at line: {} {}", self.line, c)),
+        }
         todo!()
+    }
+
+    fn advance(self: &mut Self) -> char {
+        let c = self.source.as_bytes()[self.current];
+        self.current += 1;
+
+        c as char
+    }
+
+    fn add_token(self: &mut Self, token_type: TokenType){
+        self.add_token_lit(token_type, None);
+    }
+
+    fn add_token_lit(self: &mut Self, token_type: TokenType,  literal: Option<LiteralValue>){
+        
+        let mut text = "".to_string();
+        let bytes = self.source.as_bytes();
+        for i in self.start..self.current {
+            text.push(bytes[i] as char);
+        }
+
+        self.tokens.push(Token {
+            token_type: token_type,
+            lexeme: text, 
+            literal: literal,
+            line_number: self.line
+        });
     }
 }
 
@@ -55,7 +111,7 @@ pub enum TokenType {
     Plus,
     Semicolon,
     Slash,
-    Start,
+    Star,
 
     Bang,
     BangEqual,
